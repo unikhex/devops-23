a57e56272a1a2e70ea2699108e0588bbec2cd671
Zion Awino <awinozion85@gmail.com>
2023-08-30 13:30:43 +0200

    first question in kruskompedium pg 9-10

---
+++ python kurs/ovning/kurskompedium_9_10.py
@@ -0,0 +1,19 @@
+#Fråga 1
+#Tilldelning av heltal
+a =  5
+
+#Tilldelning av sträng
+b = "5"
+c = "a"
+
+# Tilldelning av värdet av annan variabel
+
+d = a
+
+#Vad skrivas ut här ?
+print (d)
+
+#Answer
+# what is shown becomes 5
+
+
