2ad26f0872fc375f54a0ea5677550055387b4a49
Zion Awino <awinozion85@gmail.com>
2023-08-30 11:36:45 +0200

    first commit

---
+++ python kurs/lektion 2/operationer.py
@@ -0,0 +1,12 @@
+#operationer
+division = 50 / 5
+print(division)
+
+divisor = 50 // 5
+print(divisor)
+
+#typ omvandla till heltal
+print(int(3.1))
+
+#typ omvandla till flyttal
+print(float("3.112"))
