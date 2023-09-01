b18d210f4f2ec1477396455b4f7d8acede8008bf
Zion Awino <awinozion85@gmail.com>
2023-09-01 12:52:02 +0200

    another excise for the day. and changes in another file

---
+++ python kurs/lektion 3/ovning_3_2.py
@@ -0,0 +1,25 @@
+name_1 = input("What is yout name: ")
+age_1 = int(input("What is your age: "))
+
+if age_1 == 1:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1 , "sova minst 14 timmar" )
+elif age_1 == 2:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 13 timmar" )
+elif age_1 == 3:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 12 timmar" )
+elif age_1 == 4:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 11,5 timmar" )
+elif age_1 == 5 and age_1 == 6:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 11 timmar" )
+elif age_1 == 7:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 10,5 timmar" )
+elif age_1 == 8 and age_1 == 9 and age_1 == 10:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 10 timmar" )
+elif age_1 == 11:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 9,5 timmar" )
+elif age_1 >= 12 or age_1 <= 15:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 9 timmar" )
+elif age_1 == 16:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 8,5 timmar" )
+else:
+    print("Halå", name_1,". Enligt Vårdguidens rekommendaktioner behöver individer i din ålder",age_1, "sova minst 8 timmar" )
